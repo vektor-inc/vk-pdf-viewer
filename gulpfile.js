b17faf768133727de9a2da34901a8236448ae473
mtdkei <mt@matsudakeinoMacBook-Air.local>
2024-10-30 10:49:58 +0900

    Fix: src

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,32 @@
+
+/*-------------------------------------*/
+/*  Copyright
+/*-------------------------------------*/
+/*  Font
+/*-------------------------------------*/
+/*  media posts
+/*-------------------------------------*/
+/*  term color
+/*-------------------------------------*/
+/*  Font Awesome
+/*-------------------------------------*/
+/*  vk-mobile-nav
+/*-------------------------------------*/
+/*  vk-mobile-fix-nav
+/*-------------------------------------*/
+/*  page-header
+/*-------------------------------------*/
+
+
 var gulp = require('gulp');
 
 /**
  * VK PDF Viewer
  */
-gulp.task('copy_pdfjs', function(done) {
-	gulp.src('./node_modules/pdfjs-dist/build/pdf.min.js')
-		.pipe(gulp.dest('./src/assets/js/'));
-	gulp.src('./node_modules/pdfjs-dist/build/pdf.worker.min.js')
-		.pipe(gulp.dest('./src/assets/js/'));
-	done();
+gulp.task('copy_pdf', function(done) {
+    gulp.src('./node_modules/pdfjs-dist/build/pdf.min.js')
+        .pipe(gulp.dest('./src/assets/js/'))
+    gulp.src('./node_modules/pdfjs-dist/web/pdf_viewer.css')
+        .pipe(gulp.dest('./src/assets/css/'))
+    done();
 });
